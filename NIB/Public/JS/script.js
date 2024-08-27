c9074c04eb800ace08830c7df1e41e8e4920ba61
ranaslait <125487372+ranaslait@users.noreply.github.com>
2024-08-27 12:48:44 +0300

    chat

--- NIB/Public/JS/script.js
+++
@@ -1,8 +0,0 @@
-function toggleChat() {
-    const chatBox = document.getElementById('chat-box');
-    if (chatBox.style.transform === 'translateY(0%)') {
-        chatBox.style.transform = 'translateY(100%)';
-    } else {
-        chatBox.style.transform = 'translateY(0%)';
-    }
-}
